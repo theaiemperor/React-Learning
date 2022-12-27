5bd432dccd1e23d766f92aead9f9fe6b9d262f01
Arman <theaiemperor>
2022-12-27 19:08:46 +0530

    NewsWebsite-Project2--GettingOfflineData-FetchApi-----9

--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -5,49 +5,25 @@ import './pagecontent.css'
 
 export class PageContent extends Component {
 
-    pagecontent = [{
-        "heading": "heading_1",
-        "id": "XDFSS10001",
-        "imageUrl": "image1.png",
-        "content": " this is a content1. Lorem ipsum dolor, sit amet corporis, asperiores voluptates maiores explicabo dignissimos quis saepe iusto aliquam maxime? Optio ipsa doloremque, harum magnam, eaque eligendi quos delectus similique minus necessitatibus est nostrum repudiandae porro ratione cupiditate! Ex nostrum quaerat, accusamus, ut, fuga soluta temporibus molestias facere expedita eaque ipsum corporis aliquid."
-    },
-    {
-        "heading": "heading_2",
-        "id": "XDFSS6668",
-        "imageUrl": "image2.png",
-        "content": " this is a content2. Lorem ipsum dolor, sit amet corporis, asperiores voluptates maiores explicabo dignissimos quis saepe iusto aliquam maxime? Optio ipsa doloremque, harum magnam, eaque eligendi quos delectus similique minus necessitatibus est nostrum repudiandae porro ratione cupiditate! Ex nostrum quaerat, accusamus, ut, fuga soluta temporibus molestias facere expedita eaque ipsum corporis aliquid."
-    },
-    {
-        "heading": "heading_3",
-        "id": "XDFSS5003",
-        "imageUrl": "image3.png",
-        "content": " this is a content3. Lorem ipsum dolor, sit amet corporis, asperiores voluptates maiores explicabo dignissimos quis saepe iusto aliquam maxime? Optio ipsa doloremque, harum magnam, eaque eligendi quos delectus similique minus necessitatibus est nostrum repudiandae porro ratione cupiditate! Ex nostrum quaerat, accusamus, ut, fuga soluta temporibus molestias facere expedita eaque ipsum corporis aliquid."
-    },
-    {
-        "heading": "heading_4",
-        "id": "XDFSS4004",
-        "imageUrl": "image4.png",
-        "content": " this is a content4. Lorem ipsum dolor, sit amet corporis, asperiores voluptates maiores explicabo dignissimos quis saepe iusto aliquam maxime? Optio ipsa doloremque, harum magnam, eaque eligendi quos delectus similique minus necessitatibus est nostrum repudiandae porro ratione cupiditate! Ex nostrum quaerat, accusamus, ut, fuga soluta temporibus molestias facere expedita eaque ipsum corporis aliquid."
-    },
-    {
-        "heading": "heading_5",
-        "id": "XDFSS3338",
-        "imageUrl": "image5.png",
-        "content": " this is a content5. Lorem ipsum dolor, sit amet corporis, asperiores voluptates maiores explicabo dignissimos quis saepe iusto aliquam maxime? Optio ipsa doloremque, harum magnam, eaque eligendi quos delectus similique minus necessitatibus est nostrum repudiandae porro ratione cupiditate! Ex nostrum quaerat, accusamus, ut, fuga soluta temporibus molestias facere expedita eaque ipsum corporis aliquid."
-    },
-    {
-        "heading": "heading_6",
-        "id": "XDFSS2863",
-        "imageUrl": "image6.png",
-        "content": " this is a content6. Lorem ipsum dolor, sit amet corporis, asperiores voluptates maiores explicabo dignissimos quis saepe iusto aliquam maxime? Optio ipsa doloremque, harum magnam, eaque eligendi quos delectus similique minus necessitatibus est nostrum repudiandae porro ratione cupiditate! Ex nostrum quaerat, accusamus, ut, fuga soluta temporibus molestias facere expedita eaque ipsum corporis aliquid."
-    }
-    ]
+
+    pagecontent = []
 
     constructor() {
         super();
         this.state = {
             contentofpage: this.pagecontent
         }
+    }
+
+    componentDidMount(){
+        let url = "../newsdata.json";
+
+        fetch(url).then((response) => {
+            return response.text();
+        }).then((data) => {
+            let data2 = JSON.parse(data);
+            this.setState({contentofpage : data2})
+        })
     }
 
     render() {
