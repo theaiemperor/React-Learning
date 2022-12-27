6f283f019a6a7425e40b8d35300bf1e425fc2638
Arman <theaiemperor>
2022-12-27 19:03:36 +0530

    NewsWebsite-Project2--Creating-first-Classbased-component-------6

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import Navbar from './components/Navbar';
-import TextCapital from './components/TextCapital'
-import About from './components/About';
-import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+import './App.css';
 
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <>
-        <Router>
-        <Navbar title="NB" provides="Products" />
-            <Routes>
-            <Route exact path="/" element={<TextCapital/>} />
-            <Route exact path="/about" element={<About />} />
-            </Routes>
-        </Router>
-    </>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>This is my first class based component</div>
+    )
+  }
 }
-
-export default App;
