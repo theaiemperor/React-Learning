ca52477b264145b5816e058ce0c899949cbfafc2
Arman <theaiemperor>
2022-12-29 17:29:19 +0530

    P3-L7--Sending-SignIn-authenticationToken

--- Backend/routes/authentication.js
+++ Backend/routes/authentication.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 
 // We will insert an array into post request for creating validations
-router.post("/",
+router.post("/signUp",
     [
         body('name', "Enter a valid name").isLength({ min: 3 }),
         body('password').isLength({ min: 3 }),
@@ -31,7 +31,7 @@ router.post("/",
 
             // Using password Hashing
             const passwordSalt = await bcrypt.genSalt(10);
-            const securePassword = await bcrypt.hash(req.body.password , passwordSalt);
+            const securePassword = await bcrypt.hash(req.body.password, passwordSalt);
 
             user = await User.create({
                 name: req.body.name,
@@ -41,12 +41,53 @@ router.post("/",
 
             // Creating User Authentication token
             const JWT_SECRET = "This is a secret string for signing jwt token.";
-            const data = { user : {id : user.id}}
-            const authenticationToken = jwt.sign(data,JWT_SECRET);
+            const data = { user: { id: user.id } }
+            const authenticationToken = jwt.sign(data, JWT_SECRET);
 
 
 
 
+
+            res.status(200).json({ authenticationToken });
+
+        } catch (error) {
+            res.status(500).send("Backend Error")
+        }
+    })
+
+
+
+// Authenticating an user without login
+router.post("/signIn",
+    [
+        body('password', "Password cannot be blank").exists(),
+        body('email').isEmail(),
+    ],
+
+    async (req, res) => {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+        const {email , password} = req.body;
+
+        try {
+            let user = await User.findOne({ email});
+            if (!user) {
+                return res.status(400).json({ error: "Invalid credentials" })
+            }
+
+
+            const passwordCompare = await bcrypt.compare(password, user.password);
+            if (!passwordCompare) {
+                return res.status(400).json({ error: "Invalid credentials" })
+            }
+
+            const JWT_SECRET = "This is a secret string for signing jwt token.";
+            const data = { user: { id: user.id } }
+            const authenticationToken = jwt.sign(data, JWT_SECRET);
 
             res.status(200).json({ authenticationToken });
 
