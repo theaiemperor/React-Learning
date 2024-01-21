39a35a0256bd7fb77ae266ec957a64c1f0647503
Arman <theaiemperor@gmail.com>
2024-01-21 17:10:24 +0530

    Using New Redux setup

--- src/state/store.js
+++ src/state/store.js
@@ -1,5 +1,8 @@
-import {applyMiddleware, createStore} from "redux";
-import {reducers} from "./reducers";
-import {thunk} from "redux-thunk";
+import { configureStore } from '@reduxjs/toolkit';
+import { reducers } from './reducers';
+import {thunk} from 'redux-thunk';
 
-export const store = createStore(reducers, {}, applyMiddleware(thunk))
+export const store = configureStore({
+    reducer: reducers,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+});
