553155a482704e287215107d6cddb13ef554e798
Arman <theaiemperor@gmail.com>
2024-01-21 16:51:00 +0530

    Binding actions

--- src/Components/Actions.js
+++ src/Components/Actions.js
@@ -1,6 +1,11 @@
 import '../Styles/Actions.css'
+import {useDispatch} from "react-redux";
+import {actionCreators} from "../state/index"
+import {bindActionCreators} from "redux";
 
 export default function Actions() {
+    const dispatch = useDispatch();
+    const {subtract} = bindActionCreators(actionCreators, dispatch);
     return (
         <>
             <div id="container">
@@ -8,10 +13,20 @@ export default function Actions() {
                     Click on buttons to increase or decrease the balance by 50$
                 </div>
                 <div id="buttons">
-                    <button id="add" className="button">
+
+                    {/* First Method */}
+                    <button id="add" className="button"
+                            onClick={() => {
+                                dispatch(actionCreators.add(50))
+                            }}>
                         Add
                     </button>
-                    <button id="subtract" className="button">
+
+                    {/* Second Method */}
+                    <button id="subtract" className="button"
+                            onClick={() => {
+                                subtract(50)
+                            }}>
                         Subtract
                     </button>
                 </div>
--- src/Components/Balance.js
+++ src/Components/Balance.js
@@ -1,9 +1,11 @@
 import '../Styles/Balance.css'
+import {useSelector} from "react-redux";
 export default function Balance() {
+    const amount = useSelector(state => state.amount)
     return(
         <>
             <div id="balance">
-                Your Balance : 500$
+                Your Balance : {amount} $
             </div>
         </>
     )
